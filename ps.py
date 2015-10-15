76d734f6fbecccfd9e79664e353601642eae5d28
felipemiramontesr <felipemiramontesr@gmail.com>
2015-10-15 12:22:26 -0500

    Modificaciones en los rangos IP

--- ps.py
+++ ps.py
@@ -1,13 +1,38 @@
 #!usr/bin/python
 import GeoIP 
-for ra in range(1,256):
+import time
+
+from sys import argv
+
+tic = time.clock()
+#file = open('data.txt','w+')
+
+for ra in range(177,178):
 	for rb in range(0,256):
 		for rc in range(0,256):
 			for rd in range(0,256):
-				gi = GeoIP.new(GeoIP.GEOIP_MEMORY_CACHE)
 				a=str(ra)
 				b=str(rb)
 				c=str(rc)
 				d=str(rd)
-				print('IP-> '+a+'.'+b+'.'+c+'.'+d+' was localized in:-> '
-					+gi.country_name_by_addr(a+'.'+b+'.'+c+'.'+d))
+				gi = GeoIP.new(GeoIP.GEOIP_MEMORY_CACHE)
+				#print('IP-> '+a+'.'+b+'.'+c+'.'+d+' was localized in:-> '	
+					#+gi.country_name_by_addr(a+'.'+b+'.'+c+'.'+d))	
+#file.close()
+toc = time.clock()
+
+processTimeSeconds = toc - tic
+processTimeMinutes = (processTimeSeconds / 60)
+processTimeHours = (processTimeSeconds / 60) / 60
+
+processTimeDays = ((processTimeSeconds / 60) / 60) / 24
+
+processTimeString = str(processTimeSeconds)
+processTimeStringMinutes = str(processTimeMinutes)
+processTimeStringHours = str(processTimeHours)
+processTimeStringDays = str(processTimeDays)
+
+print('Process time: ' + processTimeString + ' Seconds, '
+	  + processTimeStringMinutes + ' Minutes, ' 
+	  + processTimeStringHours + ' Hours, '
+	  + processTimeStringDays + ' Days.')
